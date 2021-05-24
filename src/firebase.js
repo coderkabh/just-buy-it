7a9a458fbeacb2a8b3251cac12b8a850c320946a
Ananya jain <58131713+Ananya015@users.noreply.github.com>
2021-05-24 15:02:54 +0530

    Update firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBP3lapuXA6evdgQkepoOOontsdDLmLJzM",
+  authDomain: "just-buy-it-a19ae.firebaseapp.com",
+  projectId: "just-buy-it-a19ae",
+  storageBucket: "just-buy-it-a19ae.appspot.com",
+  messagingSenderId: "878917956308",
+  appId: "1:878917956308:web:fb5088467f1f78e7fb8141",
+  measurementId: "G-28JR8L34T8"
+};

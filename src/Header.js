8a83ac7223816f0bda04e1729ca1fd841c56d231
Ananya015 <jainananya78@gmail.com>
2021-07-20 17:10:21 +0530

    added hooks for basket

--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,14 @@ import { auth } from './firebase';
 
 
 function Header() {
+    const [{basket, user}, dispatch] = useStateValue();
+
+    const handleAuthentication = () => {
+        if(user){
+            auth.signOut();
+
+        }
+    }
     return (
         <div className="header">
         <Link to="/">

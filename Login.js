0b572d786718598587a6573b6baebada9c12bf90
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-07-20 22:31:37 +0530

    Added firebase methods

--- Login.js
+++ Login.js
@@ -5,7 +5,28 @@ function Login() {
     const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
+
+    const signIn = (e) =>{
+        e.preventDefault();
+        auth
+        .signInWithEmailAndPassword(email, password)
+        .then( auth =>{
+            history.push('./')
+        })
+        .catch(error => alert(error.message));
+    }
+
+    const register = (e) =>{
+        auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((auth) =>{
+            console.log(auth);
+            if(auth){
+                history.push('./');
+            }
+        })
+        .catch(error => alert(error.message))
+
     return (
         <div className="login">
             <Link to="/">

19baee6364260ae0cd9bb47974fbbe16451b3baf
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-07-20 22:31:59 +0530

    optimized imports

--- Login.js
+++ Login.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Login.css'
+import {Link, useHistory} from 'react-router-dom'
+import {auth} from './firebase.js'
 
 function Login() {
 

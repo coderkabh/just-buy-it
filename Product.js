864d844313bb712cb93b1ffe76b6909a14e3a5b5
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-07-20 22:36:13 +0530

    Created Product basic skeleton

--- Product.js
+++ Product.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 function Product() {
     return (
-        <div>
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>₹</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i)=> (
+                        <p>⭐</p>
+                     ))}
+
+                   
+                </div>
+            </div>
             
+            <img src={image} />
+
+            <button onClick={addToCart}>Add to Cart</button>
         </div>
     )
 }

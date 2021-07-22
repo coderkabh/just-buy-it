76664978fa5a43208b09f41375d04f04f8004bfe
Ananya015 <jainananya78@gmail.com>
2021-07-22 23:37:46 +0530

    added product div

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,9 +2,30 @@ import React from 'react'
 
 function Checkout() {
     return (
-        <div>
-            
+        <div className="checkout">
+        <div className="checkout__left">
+            <img className="checkout__ad" src={imgPath} />
+            <div>
+                <h3>{`Hello, ${user?.email}`}</h3>
+                <h2 className="checkout__title">Your Shopping Cart</h2>
+                {basket.map(item =>(
+                <CheckoutProduct
+                id = {item.id}
+                title = {item.title}
+                image = {item.image}
+                price = {item.price}
+                rating = {item.rating}
+                />
+                ))}
+            </div>
         </div>
+        <div className="checkout__right">
+            <Subtotal/>
+            <h2>The sbt will go here</h2>
+        </div>
+
+    </div>
+
     )
 }
 

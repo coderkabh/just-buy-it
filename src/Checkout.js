7bd46693990de8731169ca4cb9353fdb5461effa
Ananya015 <jainananya78@gmail.com>
2021-07-22 23:40:01 +0530

    added css file

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Checkout.css'
 import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';

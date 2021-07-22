6cebcc240207d22bb697c790cd4d9a2b11a785dd
Ananya015 <jainananya78@gmail.com>
2021-07-22 23:55:42 +0530

    added reducer

--- src/reducer.js
+++ src/reducer.js
@@ -6,3 +6,37 @@ export const initialState = {
 
 export const getCartTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0);
   
+const reducer = (state, action) =>{
+    console.log(action);
+    switch(action.type){
+        case 'ADD_TO_CART': 
+            return {
+                ...state,
+                basket: [...state.basket, action.item]
+            };
+        case 'REMOVE_FROM_CART':
+            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id );
+
+            let newBasket = [...state.basket];
+            if (index >= 0){
+                newBasket.splice(index, 1);
+
+            }else{
+                console.warn(`Can not remove product of id ${action.id} as it is not in the basket`);
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+
+        case 'SET_USER':
+            return{
+                ...state,
+                user: action.user
+            }
+        default:
+            return state;
+    }
+};
+

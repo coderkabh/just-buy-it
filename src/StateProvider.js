b3962ed47d16a62e9ad9df42492e09b72bd5da45
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-07-23 22:22:45 +0530

    done StateProvider.js

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -7,3 +7,5 @@ export const StateProvider = ({reducer, initialState, children }) =>(
         {children}
         </StateContext.Provider>
 );
+
+export const useStateValue = () => useContext(StateContext);

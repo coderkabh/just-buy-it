bcfcb3791dcd325fc8b5454944c50e8558463472
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-07-23 22:25:05 +0530

    added props

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './CheckoutProduct.css'
 import { useStateValue } from './StateProvider';
-function CheckoutProduct() {
+function CheckoutProduct({id, image, title, price, rating}) {
     return (
         <div>
             

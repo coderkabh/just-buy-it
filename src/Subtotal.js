f28b7a36e285d4a7a7eec7a1e614700e02d366b9
Ananya015 <jainananya78@gmail.com>
2021-07-24 00:19:24 +0530

    subtotal section

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,8 +4,27 @@ function Subtotal() {
     const history = useHistory();
     const [{basket}, dispatch] = useStateValue();
     return (
-        <div>
-            
+        <div className="subtotal">
+            <CurrencyFormat
+                renderText = {(value) =>(
+                    <>
+                        <p>
+                            Subtotal ({basket.length} items): <strong> {value} </strong>
+                        </p>
+                        
+                        <small className="subtotal__gift">
+                            <input type="checkbox"/> This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value= { getCartTotal(basket) }
+                displayType = {"text"}
+                thousandSeperator={true}
+                prefix={"₹"}
+            />
+           
+            <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
         </div>
     )
 }

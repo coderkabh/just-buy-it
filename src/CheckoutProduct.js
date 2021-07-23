36b054f768c1fbc20afd48c7e2266b355fcfced6
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-07-23 22:25:33 +0530

    used state from State Provider

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,6 +2,15 @@ import React from 'react'
 import './CheckoutProduct.css'
 import { useStateValue } from './StateProvider';
 function CheckoutProduct({id, image, title, price, rating}) {
+
+    const [{basket}, dispath] = useStateValue();
+    const removeFromCart = () => {
+        dispath({
+            type: 'REMOVE_FROM_CART',
+            id: id,
+        })
+    }
+
     return (
         <div>
             
